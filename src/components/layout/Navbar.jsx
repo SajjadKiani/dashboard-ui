12f72c982048a3afba19d7b8d20c0cf5e4ba56a5
saji <skm.kiani@gmail.com>
2024-02-05 15:14:12 +0330

    add theme

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,7 @@
 import Logo from "./Logo";
 import { Button } from "../ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import ModeToggle from "../common/ModeToggle";
 
 export default function Navbar () {
 
@@ -8,11 +9,13 @@ export default function Navbar () {
         <header className="flex justify-between items-center">
             <Logo />
 
-            <div>
+            <div className="flex gap-3">
                 <Avatar>
                     <AvatarImage src="https://avatars.githubusercontent.com/u/78721995?v=4" alt="@shadcn" />
                     <AvatarFallback>SK</AvatarFallback>
                 </Avatar>
+
+                <ModeToggle />
             </div>
         </header>
     )
