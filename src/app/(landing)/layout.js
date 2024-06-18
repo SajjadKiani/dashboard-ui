8ceb4cef799f582469c6a2f4a6568e576b7544ef
saji <skm.kiani@gmail.com>
2024-06-18 16:41:14 +0330

    landing page nav

--- src/app/(landing)/layout.js
+++ src/app/(landing)/layout.js
@@ -1,10 +1,11 @@
-
+import { Navbar } from "@/components/layout/landing";
+// import Navbar from "@/components/layout/Navbar"
 
 export default function Layout ({children}) {
     
-
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen flex flex-col">
+            <Navbar />
             {children}
         </div>
     )
