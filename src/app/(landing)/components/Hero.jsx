bdc4435426d190cfa0771e40cfc6668580ddd450
saji <skm.kiani@gmail.com>
2024-06-20 12:06:36 +0330

    update content

--- src/app/(landing)/components/Hero.jsx
+++ src/app/(landing)/components/Hero.jsx
@@ -37,7 +37,7 @@ export const Hero = () => {
 
           <a
             rel="noreferrer noopener"
-            href="https://github.com/leoMirandaa/shadcn-landing-page.git"
+            href="https://github.com/sajjadkiani/dashboard-ui"
             target="_blank"
             className={`w-full md:w-1/3 ${buttonVariants({
               variant: "outline",
